64bf71b294629bc79f09fe83aea39660eb07d1e5
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2025-09-21 15:42:22 +0600

    added more files

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Finance Tracker API is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI).then
+(()=>{console.log("Mongodb connected");
+    app.listen(process.env.PORT||5000,()=>{
+        console.log(`server running on port ${process.env.PORT || 5000}`);
+    });
+}).catch(err=>console.error(err));
