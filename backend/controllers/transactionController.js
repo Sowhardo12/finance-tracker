dac742b73b99d0be0009685eb8b70dcb0071972c
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2025-09-22 16:51:02 +0600

    added Update and Delete option for Transactions, Now fully CRUD backend

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -26,3 +26,50 @@ exports.getTransactions = async (req,res)=>{
         res.status(500).json({error: err.message});
     }
 };
+
+//updating the transaction, adding new fields in the same transaction 
+exports.updateTransactions = async (req, res)=>{
+    try{
+        const { id } = req.params;
+        //parameter comes in the link
+
+        //finding the transaction
+        //transaction -> the specific transaction
+        //Transaction -> the mongodb model 
+        let transaction = await Transaction.findById(id);
+        if(!transaction){
+            return res.status(404).json({message:"Transaction not found"});
+        }
+        //check if the transaction is owned by the used sending the HTTP request
+        if(transaction.user.toString()!==req.user){
+            return res.status(401).json({message:"Not Authorised"});
+        }
+
+        //update with new fields
+        transaction = await Transaction.findByIdAndUpdate(
+            id,req.body,{new:true, runValidators:true}
+        );
+        res.json(transaction);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+};
+
+//deleting transaction
+exports.deleteTransaction = async(req,res)=>{
+    try{
+        const{id} = req.params;
+        const transaction = await Transaction.findById(id);
+        if(!transaction){
+            return res.status(404).json({message:"Transaction Not Found"})
+        }
+        //enrusing ownership (if transaction done by the requested user)
+        if(transaction.user.toString()!== req.user){
+            res.status(401).json({message:"Not Authorized"});
+        }
+        await transaction.deleteOne(); //deletes that transaction from the database
+        res.json({message:"Transaction deleted successfully"});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const {addTransaction,getTransactions} = require("../controllers/transactionController");
+const {
+        addTransaction,
+        getTransactions,
+        updateTransactions,
+        deleteTransaction
+      } = require("../controllers/transactionController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/",authMiddleware,addTransaction);
 router.get("/",authMiddleware,getTransactions);
+router.put("/:id",authMiddleware,updateTransactions);
+router.delete("/:id",authMiddleware,deleteTransaction);
 //in both cases, the authMiddleware runs first, checks the JWT token and attaches the req.user
 //the req.user then used to create transaction entry with that user
 
